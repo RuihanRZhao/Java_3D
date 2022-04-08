29d8608746aa687747043927707dc3facd03fbcd
Ruihan Zhao <fouxzhao@gmail.com>
2022-04-08 12:01:52 +0100

    2D version engine finished

--- src/Engine/camera.java
+++ src/Engine/camera.java
@@ -4,19 +4,18 @@ import System.IO;
 
 import java.awt.event.*;
 
-public class camera implements KeyListener, MouseListener, MouseMotionListener {
+public class camera implements KeyListener{
     public static position P;
-    public static boolean left;
-    public static boolean right;
-    public static boolean forward;
-    public static boolean back;
     public boolean Jump;
+    public static MotionTrend MT;
+
     public static final double MOVE_SPEED = .08;
     public static final double ROTATION_SPEED= .045;
     public static double x_Rotate, y_Rotate;
     public static double mouse_x, mouse_y;
     public camera(position input){
-        this.P=input;
+        this.P = input;
+        this.MT=new MotionTrend();
     }
 
     @Override
@@ -27,121 +26,37 @@ public class camera implements KeyListener, MouseListener, MouseMotionListener {
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
-            case (KeyEvent.VK_A) -> left = true;
-            case (KeyEvent.VK_D) -> right = true;
-            case (KeyEvent.VK_W) -> forward = true;
-            case (KeyEvent.VK_S) -> back = true;
+            case (KeyEvent.VK_A) -> MT.move.left = true;
+            case (KeyEvent.VK_D) -> MT.move.right = true;
+            case (KeyEvent.VK_W) -> MT.move.up = true;
+            case (KeyEvent.VK_S) -> MT.move.down = true;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
-            case (KeyEvent.VK_A) -> left = false;
-            case (KeyEvent.VK_D) -> right = false;
-            case (KeyEvent.VK_W) -> forward = false;
-            case (KeyEvent.VK_S) -> back = false;
+            case (KeyEvent.VK_A) -> MT.move.left = false;
+            case (KeyEvent.VK_D) -> MT.move.right = false;
+            case (KeyEvent.VK_W) -> MT.move.up = false;
+            case (KeyEvent.VK_S) -> MT.move.down = false;
         }
     }
 
-
-
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        x_Rotate=0;
-        y_Rotate=0;
-        int x = e.getX(), y = e.getY();
-        x_Rotate = mouse_x - x;
-        y_Rotate = mouse_y - y;
-        mouse_x=x;
-        mouse_y=y;
-        IO.print("1\n"+"x: "+mouse_x+"\ty: "+mouse_y+"\n\n");
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        x_Rotate=0;
-        y_Rotate=0;
-        int x = e.getX(), y = e.getY();
-        x_Rotate = mouse_x - x;
-        y_Rotate = mouse_y - y;
-        mouse_x=x;
-        mouse_y=y;
-        IO.print("2\n"+"x: "+mouse_x+"\ty: "+mouse_y+"\n\n");
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        x_Rotate=0;
-        y_Rotate=0;
-        int x = e.getX(), y = e.getY();
-        x_Rotate = mouse_x - x;
-        y_Rotate = mouse_y - y;
-        mouse_x=x;
-        mouse_y=y;
-        IO.print("3\n"+"x: "+mouse_x+"\ty: "+mouse_y+"\n\n");
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        x_Rotate=0;
-        y_Rotate=0;
-        int x = e.getX(), y = e.getY();
-        x_Rotate = mouse_x - x;
-        y_Rotate = mouse_y - y;
-        mouse_x=x;
-        mouse_y=y;
-        IO.print("4\n"+"x: "+mouse_x+"\ty: "+mouse_y+"\n\n");
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        x_Rotate=0;
-        y_Rotate=0;
-        int x = e.getX(), y = e.getY();
-        x_Rotate = mouse_x - x;
-        y_Rotate = mouse_y - y;
-        mouse_x=x;
-        mouse_y=y;
-        IO.print("5\n"+"x: "+mouse_x+"\ty: "+mouse_y+"\n\n");
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        x_Rotate=0;
-        y_Rotate=0;
-        int x = e.getX(), y = e.getY();
-        x_Rotate = mouse_x - x;
-        y_Rotate = mouse_y - y;
-        mouse_x=x;
-        mouse_y=y;
-        IO.print("6\n"+"x: "+mouse_x+"\ty: "+mouse_y+"\n\n");
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        x_Rotate=0;
-        y_Rotate=0;
-        int x = e.getX(), y = e.getY();
-        x_Rotate = mouse_x - x;
-        y_Rotate = mouse_y - y;
-        mouse_x=x;
-        mouse_y=y;
-        IO.print("7\n"+"x: "+mouse_x+"\ty: "+mouse_y+"\n\n");
-
-    }
     public static void update(int[][] map){
         //position change by WASD
 
-        double newX=0.0, newY=0.0;
-        if(forward) { newX=P.x.direction;  newY=P.y.direction; }
-        if(back)    { newX=-P.x.direction; newY=-P.y.direction;}
-        if(left)    { newX=-P.y.direction; newY=P.x.direction; }
-        if(right)   { newX=P.y.direction;  newY=-P.x.direction;}
-        if(forward || back || left || right){
-            if(map[(int)(P.x.position + newX * MOVE_SPEED)][(int)P.y.position] == 0) P.x.position+=newX*MOVE_SPEED;
-            if(map[(int)P.x.position][(int)(P.y.position + newY * MOVE_SPEED)] ==0) P.y.position+=newY*MOVE_SPEED;
+        if(MT.move.up)      {
+            makeMove(map, P.x.direction, P.y.direction);
+        }
+        if(MT.move.down)    {
+
+        }
+        if(MT.move.left)    {
+            makeMove(map, -P.x.direction, P.y.direction);
+        }
+        if(MT.move.right)   {
+            makeMove(map, P.x.direction, -P.y.direction);
         }
         //direction change by moving mouse
 
@@ -151,5 +66,33 @@ public class camera implements KeyListener, MouseListener, MouseMotionListener {
         double oldxPlane = P.x.plane;
         P.x.plane = P.x.plane*Math.cos(ROTATION_SPEED * x_Rotate) - P.y.plane*Math.sin(ROTATION_SPEED * x_Rotate);
         P.y.plane = oldxPlane*Math.sin(ROTATION_SPEED * x_Rotate) + P.y.plane*Math.cos(ROTATION_SPEED * x_Rotate);
+    }
+
+    public static void makeMove(int[][] map, double X, double Y){
+        if(map[(int)(P.x.position + X * MOVE_SPEED)][(int)P.y.position] == 0) P.x.position+=X*MOVE_SPEED;
+        if(map[(int)P.x.position][(int)(P.y.position + Y * MOVE_SPEED)] ==0) P.y.position+=Y*MOVE_SPEED;
+    }
+}
+class MotionTrend{
+    public direction move;
+    public direction point;
+    public MotionTrend(){
+        this.move = new direction();
+        this.point = new direction();
+    }
+}
+class direction{
+    public boolean left;
+    public boolean right;
+    public boolean up;
+    public boolean down;
+    public direction(){
+        this.left = false;
+        this.right = false;
+        this.up = false;
+        this.down = false;
+    }
+    public boolean haveTrend(){
+        return (this.left || this.right || this.down || this.up);
     }
 }
